cc3b72fe268b99dbe7974e68cf64dd757115ad3a
Jorge Meza <jorge.meza.rdz@gmail.com>
2020-01-20 19:32:13 -0600

    mod20012020

--- server/controladores/dps.js
+++ server/controladores/dps.js
@@ -1,7 +1,11 @@
-var SerialPort = require('serialport');
-var serialPort = new SerialPort('/dev/ttyUSB0', {
-  baudrate: 9600
+var serialport = require("serialport"); 
+var SerialPort = serialport.SerialPort; 
+
+var serialPort = new SerialPort("/dev/ttyUSB0", {
+  baudrate: 9600,
+  parser: serialport.parsers.readline("\n")
 });
+
 
 
 var Gpio = require('onoff').Gpio; 
